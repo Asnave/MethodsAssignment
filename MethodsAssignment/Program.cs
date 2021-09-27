588e84efb87c1bcfa048882c36cb4a462584b472
Abigail Evans <90458573+Asnave@users.noreply.github.com>
2021-09-27 14:09:39 -0300

    Damage , Add Score Update

--- MethodsAssignment/Program.cs
+++ MethodsAssignment/Program.cs
@@ -14,18 +14,34 @@ namespace MethodsAssignment
         static int xp = 0;
         static void Main(string[] args)
         {
+            ShowHUD();
+            Console.ReadKey(true);
         }
 
         static void ShowHUD()
         {
-            
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("------HUD------");
-            Console.WriteLine("Score: " + score);
-            Console.WriteLine("Health" + health);
-            Console.WriteLine("Lives" + lives);
-            Console.WriteLine("XP" + xp);
+            Console.ResetColor();
+            Console.WriteLine(" Score: " + score);
+            Console.WriteLine(" Health: " + health);
+            Console.WriteLine(" Lives: " + lives);
+            Console.WriteLine(" XP: " + xp);
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("---------------");
+            Console.ResetColor();
 
         }
+
+        static void Damage(int damage)
+        {
+            health = health - damage;
+        }
+
+        static void AddScore(int enemyValue, int ScoreMulti)
+        { 
+
+        }
+        
     }
 }
