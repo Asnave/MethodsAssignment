d0305e2fb1646a7b89d74ae212ad13cfb847d3f8
Abigail Evans <90458573+Asnave@users.noreply.github.com>
2021-09-27 13:51:20 -0300

    HUD

--- MethodsAssignment/Program.cs
+++ MethodsAssignment/Program.cs
@@ -8,8 +8,24 @@ namespace MethodsAssignment
 {
     class Program
     {
+        static int score = 0;
+        static int health = 100;
+        static int lives = 3;
+        static int xp = 0;
         static void Main(string[] args)
         {
+        }
+
+        static void ShowHUD()
+        {
+            
+            Console.WriteLine("------HUD------");
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Health" + health);
+            Console.WriteLine("Lives" + lives);
+            Console.WriteLine("XP" + xp);
+            Console.WriteLine("---------------");
+
         }
     }
 }
